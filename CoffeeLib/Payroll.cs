5c83d9cc6538643970537e332216600aee75f48b
WyoMetz <34138193+WyoMetz@users.noreply.github.com>
2019-02-10 12:54:13 -0500

    Updated Application Interface

--- CoffeeLib/Payroll.cs
+++ CoffeeLib/Payroll.cs
@@ -2,6 +2,6 @@
 {
     public struct Payroll
     {
-        public double Cost(int hours) => (hours * 12.50) + ((hours - 40) * 6.25);
+        public double Cost(int hours) => (hours - 40 > 0)?(hours * 12.50) + ((hours - 40) * 6.25) : (hours * 12.50);
     }
 }
